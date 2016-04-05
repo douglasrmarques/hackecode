8b8e8ebf72a7585bbe7a5652ea295e67989a162e
Douglas R Marques <drmarques@scopus.com.br>
2016-04-05 14:34:32 -0300

    Classe com a correção proposta pelo problema.
    Loop while não tratava a variável "J==0", de maneira que a primeira
    iteração não era realizada.

--- hackerrank/src/hackerrank/CorrectnessInvariant.java
+++ hackerrank/src/hackerrank/CorrectnessInvariant.java
@@ -4,15 +4,7 @@ import java.util.Scanner;
 
 public class CorrectnessInvariant {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] ar = new int[n];
-        for(int i=0;i<n;i++){
-            ar[i]=in.nextInt();
-        }
-        insertionSort(ar);
-	}
+	
 	
 	static void printArray(int[] ar) {
         for(int n: ar){
@@ -24,7 +16,7 @@ public class CorrectnessInvariant {
         for(int i = 1; i < A.length; i++){
             int value = A[i];
             int j = i - 1;
-            while(j > 0 && A[j] > value){
+            while(j >= 0 && A[j] > value){
                 A[j + 1] = A[j];
                 j = j - 1;
             }
@@ -33,5 +25,15 @@ public class CorrectnessInvariant {
 
         printArray(A);
     }
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int i=0;i<n;i++){
+            ar[i]=in.nextInt();
+        }
+        insertionSort(ar);
+	}
 
 }
